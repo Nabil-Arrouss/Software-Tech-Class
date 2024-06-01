5210d1878ef1adc90a4a55246c1c287af90128ae
Nabil Arrouss <nabilarrouss@gmail.com>
2024-06-01 17:37:53 +0200

    added the routing

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+
+use App\Http\Controllers\Api\V1\ImageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+/* This method organizes routes starting with '/v1' (version 1) for image management. 
+ * It supports CRUD operations but excludes image updates
+ */
+Route::group(['prefix' => 'v1'], function() {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // This line registers image management routes using 'ImageController', excluding the 'update' route.
+    Route::apiResource('images', ImageController::class)->except('update');
 });
