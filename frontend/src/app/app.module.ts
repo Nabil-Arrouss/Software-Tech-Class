017ce5373b701831b357f96b68dd660c46726910
Nabil Arrouss <nabilarrouss@gmail.com>
2024-06-01 20:21:54 +0200

    fixed errors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,35 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
-import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MuseumComponent } from './components/museum/museum.component';
+import { ImageFormComponent } from './components/image-form/image-form.component';
+import { ImageComponent } from './components/image/image.component';
+import { ImageInfoComponent } from './components/image-info/image-info.component';
 
-import { MuseumComponent } from "./components/museum/museum.component";
-
-import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, MuseumComponent],
-
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MuseumComponent,
+    ImageFormComponent,
+    ImageComponent,
+    ImageInfoComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    NgbModule,
+    NgbModule
   ],
-
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
