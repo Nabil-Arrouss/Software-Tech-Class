22d5d35048612d1d5336e837c5e3e5f8830ba45b
{Karim-Mahgoub} <{kareemhesham.230@gmail.com}>
2024-06-03 20:34:10 +0200

    unit testing

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -8,11 +8,13 @@ use Illuminate\Support\Facades\Route;
 /* This method organizes routes starting with '/v1' (version 1) for image management.
  * It supports CRUD operations but excludes image updates
  */
-Route::group(['prefix' => 'v1'], function() {
+Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function() {
 
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
     Route::delete('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-    // This line registers image management routes using 'ImageController', excluding the 'update' route.
+      // This line registers image management routes using 'ImageController', excluding the 'update' route.
     Route::apiResource('images', ImageController::class)->except('update');
+
+
 });
