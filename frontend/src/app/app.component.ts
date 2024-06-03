1dd57bba67dd93a35eab299e7f480782baba7f7c
{Karim-Mahgoub} <{kareemhesham.230@gmail.com}>
2024-06-03 15:53:22 +0200

    Auth in integrated into the project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TokenService } from "src/app/services/token.service"; // Assuming TokenService has the necessary methods
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "frontend";
+
+  constructor(private tokenService: TokenService) {}
+
+  get isAuthenticated(): boolean {
+    return this.tokenService.isAuthenticated();
+  }
 }
---
+++ frontend/src/app/models/api-responses.model.ts
@@ -0,0 +1,5 @@
+// src/app/models/api-responses.model.ts
+export interface LoginResponse {
+  token: string;
+  message?: string; // Optional message field
+}
